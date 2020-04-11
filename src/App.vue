c0664ec2d4115e18fba80f889b991a31e16a729e
OliverLiao <rtw0913@gmail.com>
2020-04-11 22:45:31 +0800

    perpare vue project and template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue CI/CD 建構自動化部署流水線 - [ 部署 ]</h1>
+    <p>寫一個加法 function 來進行單元測試</p>
+    <div class="calc">
+      <span>
+        <label for=""></label>
+        <input v-model="number1" type="number">
+      </span>
+      <span class="symbol">+</span>
+      <span>
+        <label for=""></label>
+        <input v-model="number2" type="number">
+      </span>
+      <span class="symbol">=</span>
+      <span class="result">{{ result }}</span>
+      <button @click="addition(number1, number2)">計算</button>
+      <p class="remind" v-if="isRemind">兩個參數都要輸入喔！</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      result: null,
+      number1: null,
+      number2: null,
+      isRemind: false,
+    };
+  },
+  methods: {
+    addition(n1, n2) {
+      this.isRemind = false;
+      if (n1 && n2) {
+        const input1 = parseInt(n1, 0);
+        const input2 = parseInt(n2, 0);
+        this.result = this.calc(input1, input2);
+      } else {
+        this.isRemind = true;
+      }
+    },
+    calc(n1, n2) {
+      return n1 + n2;
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.calc{
+  display: flex;
+  align-items: center;
+}
+.symbol{
+  margin: 0 10px;
+}
+.remind{
+  margin-left: 10px;
+  color: red;
+}
+.result{
+  display: inline-block;
+  width: 100px;
+  height: 35px;
+  background: #eec;
+  margin: 10px;
+  line-height: 35px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('APP unit test', () => {
+  it('addition function test', () => {
+    const wrapper = shallowMount(App);
+    // 檢查實體是否建立
+    expect(wrapper.isVueInstance()).toBe(true);
+    // 測試 calc function
+    expect(wrapper.vm.calc(3, 5)).toBe(8);
   });
 });
