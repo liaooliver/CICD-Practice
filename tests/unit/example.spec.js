639e28d378ee3de92b7874cda2d64e5b920487ea
OliverLiao <rtw0913@gmail.com>
2020-04-17 21:30:39 +0800

    modified test file

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,11 @@
+// 淺渲染
 import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 
 describe('APP unit test', () => {
   it('addition function test', () => {
     const wrapper = shallowMount(App);
-    // 檢查實體是否建立
+    // 測試 Vue 實體是否建立
     expect(wrapper.isVueInstance()).toBe(true);
     // 測試 calc function
     expect(wrapper.vm.calc(3, 5)).toBe(8);
